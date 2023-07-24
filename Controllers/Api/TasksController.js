ca1def1c4e5bf3506d7f0d793d4061d5698d6f34
AhmedHdeawy <ahmedhdeawy@gmail.com>
2023-07-24 18:45:11 +0300

    wrap with async wrapper

--- Controllers/Api/TasksController.js
+++ Controllers/Api/TasksController.js
@@ -9,64 +9,40 @@ const index = asyncWrapper(async (req, res) => {
 })
 
 
-const store = async (req, res) => {
-    try {
-        const task = await Task.create(req.body)    
-        res.status(201).json(successResponse(task, 'Task created successgully'))
-    } catch (error) {
-        res.status(500).json(errorResponse(error))
-    }
-
-}
+const store = asyncWrapper(async (req, res) => {
+    const task = await Task.create(req.body)
+    res.status(201).json(successResponse(task, 'Task created successgully'))
+})
 
 const getTask = asyncWrapper(async (req, res) => {
     const task = await Task.findById(req.params.id);
     if (!task) {
         throw modelNotFoundError(`Task with id ${req.params.id} is not found`)
-        const err = new Error(`Task with id ${req.params.id} is not found`)
-        err.statusCode = 422
-        throw err
-        return res.status(500).json(errorResponse(`Task with id ${req.params.id} is not found`))
     }
     res.status(200).json(successResponse(task))
 })
 
-const update = async (req, res) => {
-    try {
-        
-        const taskId = req.params.id;
-        const task = await Task.findOneAndUpdate(
-            { _id: taskId },
-            req.body,
-            {
-                new: true,
-                runValidators: true
-            }
-        )
-        if (!task) {
-            return res.status(500).json(errorResponse(`Task with id ${taskId} is not found`))
-        }
-        res.status(200).json(successResponse(task, 'updated successfully'))
-    } catch (error) {
-        res.status(500).json(errorResponse(error))
+const update = asyncWrapper(async (req, res) => {
+    const taskId = req.params.id;
+    const task = await Task.findOneAndUpdate(
+        { _id: taskId },
+        req.body,
+        { new: true, runValidators: true }
+    )
+    if (!task) {
+        throw modelNotFoundError(`Task with id ${req.params.id} is not found`)
     }
+    res.status(200).json(successResponse(task, 'updated successfully'))
+})
 
-}
-
-const deleteTask = async (req, res) => {
-    try {
-
-        const taskId = req.params.id;
-        const task = await Task.findOneAndDelete({ _id: taskId })
-        if (!task) {
-            return res.status(500).json(errorResponse(`Task with id ${taskId} is not found`))    
-        }
-        res.status(200).json(successResponse(task, 'deleted successfully'))
-    } catch (error) {
-        res.status(500).json(errorResponse(error))
+const deleteTask = asyncWrapper(async (req, res) => {
+    const taskId = req.params.id;
+    const task = await Task.findOneAndDelete({ _id: taskId })
+    if (!task) {
+        throw modelNotFoundError(`Task with id ${req.params.id} is not found`)
     }
-
-}
+    res.status(200).json(successResponse(task, 'deleted successfully'))
+})
 
 module.exports = {
     index,
