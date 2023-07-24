9520dc5f3a531e4ed566e053b76dcf3fcc8927cf
AhmedHdeawy <ahmedhdeawy@gmail.com>
2023-07-24 07:49:33 +0300

    handle errors using middlewarers

--- Middlewares/error-handler.js
+++ Middlewares/error-handler.js
@@ -4,7 +4,7 @@ const { ModelNotFound } = require("../utils/model-not-found")
 const errorHandler = (err, req, res, next) => {
 
     if (err instanceof ModelNotFound) {
-        res.status(err.statusCode).json(
+        return res.status(err.statusCode).json(
             errorResponse(err.message)
         )    
     }
